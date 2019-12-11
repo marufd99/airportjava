b970e063adb1fce4c31c0d1938001649a185295b
Student <maruf.d99@gmail.com>
2019-12-11 19:10:57 +0000

    Intian setup completed 1st user story plane to land in Airport completed 2nd user story plane to take off from airport failed.

---
+++ Java/airportjava/src/Plane.js
@@ -0,0 +1,12 @@
+//src/plane.js
+'use strict';
+
+function Plane(){}
+
+Plane.prototype.land = function(airport){
+  airport.clearForLanding(this);
+  this._location = airport;
+};
+Plane.prototype.takeoff = function(){
+  this._location.clearForTakeOff();
+};
